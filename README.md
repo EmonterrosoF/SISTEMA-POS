5890d285ff1f26f045a0a780c4d65e2cf22a4ff5
Edras <monterrosoedras@gmail.com>
2023-06-03 15:34:48 -0600

    sistema POS .net y angular

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sistema Punto de Venta
+
+sistema punto de venta hecho en .net u=y angular
---
+++ Utilidad/Scripts to BD/script_insert.sql
@@ -0,0 +1,27 @@
+﻿
+
+--INSERTAR ROLES
+insert into rol(descripcion,esActivo) values
+('Administrador',1),
+('Empleado',1)
+
+go
+
+--INSERTAR USUARIOS
+insert into usuario(nombreApellidos,correo,idRol,Clave,esActivo) values
+('Administrador Principal','admin@midominio.com',1,'12345',1),
+('Empleado Secundario','employe@midominio.com',2,'12345',1)
+
+go
+--INSERTAR CATEGORIAS
+insert into Categoria(descripcion,esActivo) values ('Mantequillas',1)
+insert into Categoria(descripcion,esActivo) values ('Quesos',1)
+insert into Categoria(descripcion,esActivo) values ('Yogures',1)
+insert into Categoria(descripcion,esActivo) values ('Leches',1)
+insert into Categoria(descripcion,esActivo) values ('Cremas',1)
+insert into Categoria(descripcion,esActivo) values ('Quesillo',1)
+
+go
+
+--INSERTAR NUMERO VENTA INICIAL
+insert into NumeroDocumento(ultimo_Numero) values(0)
